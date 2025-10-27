45f058cc2f3d01fdc3cbb402368e28c91b8df55e
pequesta007 <alquimis001@gmail.com>
2025-10-26 21:59:44 -0500

    1

--- menu-qr-mysql-nextjs/app/api/orders/item/route.ts
+++ menu-qr-mysql-nextjs/app/api/orders/item/route.ts
@@ -1,5 +1,5 @@
-import { prisma } from '../../../../lib/prisma'
 import { NextResponse } from 'next/server'
+import { prisma } from '../../../../lib/prisma'
 
 export async function PATCH(req: Request) {
   const body = await req.json()
